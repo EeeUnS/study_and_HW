4667323dbea4246dcaf066b0c7a1ff3f98f555c2
EeeUnS <42970615+EeeUnS@users.noreply.github.com>
2019-11-14 23:05:31 +0900

    update week2

---
+++ web_hacking/week2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,213 @@
+﻿//client
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include"RSA.h"
+#include<stdlib.h>
+//using namespace std;
+//using namespace boost::asio;
+
+const char SERVER_IP[] = "127.0.0.1";
+constexpr unsigned short PORT_NUMBER = 31400;
+
+int main()
+{
+	boost::asio::io_service io_service;
+	boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string(SERVER_IP), PORT_NUMBER);
+	boost::asio::ip::tcp::socket socket(io_service);
+	boost::system::error_code c_error;
+
+	// connect
+	socket.connect(endpoint, c_error);
+	RSA rsa;
+	if (c_error)
+	{
+		if (c_error == boost::asio::error::eof)
+			std::cout << "연결 끊김" << std::endl;
+		else
+			std::cout << "error No." << c_error.value() << ", error message" << c_error.message() << std::endl;
+		getchar();
+		return 0;
+	}
+	// =======
+	std::cout << "서버 연결 성공" << std::endl;
+
+
+	boost::system::error_code error;
+	char buffer[80];
+	//public key line = (e,n)
+	socket.read_some(boost::asio::buffer(buffer, 80), error);
+
+	int e = 0, n = 0;
+	std::cout << buffer << std::endl;
+
+	//get integer
+	int i = 0;
+	while (buffer[i] != ',')
+	{
+		if ((buffer[i] >= '0' && buffer[i] <= '9'))
+		{
+			e *= 10;
+			e += buffer[i] - '0';
+		}
+		i++;
+	}
+	while (buffer[i] != ')')
+	{
+		if ((buffer[i] >= '0' && buffer[i] <= '9'))
+		{
+			n *= 10;
+			n += buffer[i] - '0';
+		}
+		i++;
+	}
+	/////////////////////////////
+	memset(buffer, 0, 80);
+	rsa.setter_public_key(std::make_pair(e, n));
+	for (;;)
+	{
+		std::cout << "<<";
+		std::cin >> buffer;
+		rsa.Encryption(buffer);
+		if (buffer == "-1")
+		{
+			break;
+		}
+		socket.write_some(boost::asio::buffer(buffer,80), error);
+		if (error)
+		{
+			if (error == boost::asio::error::eof)
+				std::cout << "연결 끊김" << std::endl;
+			else
+				std::cout << "error no." << error.value() << ", error message:" << error.message() << std::endl;
+			break;
+		}
+		memset(buffer, 0, 80);
+	}
+	socket.close();
+	std::cout << "클라이언트 종료" << std::endl;
+	return 0;
+}
+//socket.write_some(boost::asio::buffer(&input, 1), error);
+
+//
+////클라
+//
+//#include <iostream>
+//
+//#include <boost/asio.hpp>
+//
+//
+//
+//using namespace std;
+//
+//using namespace boost::asio;
+//
+//
+//
+//const char SERVER_IP[] = "127.0.0.1";
+//
+//const unsigned short PORT_NUMBER = 31400;
+//
+//
+//
+//int main()
+//
+//{
+//
+//	io_service io_service;
+//
+//	ip::tcp::endpoint endpoint(ip::address::from_string(SERVER_IP), PORT_NUMBER);
+//
+//	ip::tcp::socket	  socket(io_service);
+//
+//	boost::system::error_code c_error;
+//
+//
+//
+//	// connect
+//
+//	socket.connect(endpoint, c_error);
+//
+//	if (c_error)
+//
+//	{
+//
+//		if (c_error == error::eof)
+//
+//			cout << "연결 끊김" << endl;
+//
+//		else
+//
+//			cout << "error No." << c_error.value() << ", error message" << c_error.message() << endl;
+//
+//		getchar();
+//
+//		return 0;
+//
+//	}
+//
+//	// =======
+//
+//	cout << "서버 연결 성공" << endl;
+//
+//	for (int i = 0; i < 7; i++)
+//
+//	{
+//
+//		char buf[128] = { 0 };
+//
+//		sprintf_s(buf, 128 - 1, "%d 보냄", i);
+//
+//		int len_buf = strnlen_s(buf, 128 - 1);
+//
+//		boost::system::error_code error;
+//
+//		socket.write_some(buffer(buf, len_buf), error);
+//
+//		cout << "보낸 메시지 : " << buf << endl;
+//
+//
+//
+//		char r_buf[128] = { 0 };
+//
+//		boost::system::error_code r_error;
+//
+//		socket.read_some(buffer(r_buf), r_error);
+//
+//		if (r_error)
+//
+//		{
+//
+//			if (r_error == error::eof)
+//
+//				cout << "연결 끊김" << endl;
+//
+//			else
+//
+//				cout << "error No." << r_error.value() << ", error message" << r_error.message() << endl;
+//
+//			break;
+//
+//		}
+//
+//		cout << "서버로부터 : " << r_buf << endl;
+//
+//	}
+//
+//	if (socket.is_open())
+//
+//	{
+//
+//		socket.close();
+//
+//	}
+//
+//	cout << "클라이언트 종료" << endl;
+//
+//
+//
+//	return 0;
+//
+//}
+//
---
+++ web_hacking/week2/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,138 @@
+﻿//// 서버
+
+#include"RSA.h"
+#include<iostream>
+#include<boost/asio.hpp>
+#include<string>
+
+//using namespace std;
+//using namespace boost::asio;
+
+const char SERVER_ip[] = "127.0.0.1";
+constexpr std::size_t PORT_NUMBER = 31400;
+
+//https://heurinbada.tistory.com/105
+
+int main()
+{
+	std::cout << "접속 대기중" << std::endl;
+	int number = 1;
+	std::unique_ptr<boost::asio::io_service> _io_service = std::make_unique< boost::asio::io_service>();
+	boost::asio::ip::tcp::endpoint	endpoint(boost::asio::ip::tcp::v4(), static_cast<unsigned short>(PORT_NUMBER));
+	boost::asio::ip::tcp::acceptor acceptor(*_io_service, endpoint);
+	boost::asio::ip::tcp::socket socket(*_io_service);
+
+	acceptor.accept(socket);//접속대기
+	std::cout << "연결" << "PORT_NUMBER : " << PORT_NUMBER  << std::endl;
+
+	RSA rsa;
+	rsa.generate_key(17, 7);
+	auto key = rsa.getter_public_key();
+
+	std::string line = '(' + std::to_string(key.first) + ',' + std::to_string(key.second) + ')';
+	boost::system::error_code error;
+	socket.write_some(boost::asio::buffer(line, line.length()), error);
+
+	for (;;)
+	{
+		char read_line[80] = {0,};
+		socket.read_some(boost::asio::buffer(read_line,80), error);//읽기 대기
+
+		if (error)
+		{
+			if (error == boost::asio::error::eof)
+				std::cout << "연결 끊김" << std::endl;
+			else
+				std::cout << "error no." << error.value() << ", error message:" << error.message() << std::endl;
+			break;
+		}
+		std::cout << "Encrypt >> "<< read_line << '\n';
+		rsa.Decryption(read_line);
+		std::cout << ">> " << read_line << '\n';
+
+	}
+	std::cout << "  연결끊김 " << std::endl;
+
+
+
+
+	return 0;
+}
+//
+//#include <iostream>
+//
+//#include <boost/asio.hpp>
+//
+//using namespace std;
+//
+//using namespace boost::asio;
+//
+//
+//
+//const char SERVER_IP[] = "127.0.0.1";
+//
+//const unsigned short PORT_NUMBER = 31400;
+//
+//
+//
+//int main()
+//{
+//	io_service io_service;
+//	ip::tcp::endpoint	endpoint(ip::tcp::v4(), PORT_NUMBER);
+//	ip::tcp::acceptor	acceptor(io_service, endpoint);
+//	ip::tcp::socket		socket(io_service);
+//	acceptor.accept(socket);
+//
+//	cout << "클라이언트 연결" << endl;
+//
+//	for (;;)
+//	{
+//		char buf[128] = { 0 };
+//		boost::system::error_code error;
+//		socket.read_some(buffer(buf), error);
+//
+//		if (error)
+//		{
+//
+//			if (error == error::eof)
+//
+//				cout << "연결 끊김" << endl;
+//
+//			else
+//
+//				cout << "error No." << error.value() << ", error message:" << error.message() << endl;
+//
+//			break;
+//
+//		}
+//
+//		cout << "클라이언트로부터 : " << buf << endl;
+//
+//
+//
+//		char r_buf[128] = { 0 };
+//
+//		boost::system::error_code r_error;
+//
+//
+//
+//		sprintf_s(r_buf, 128 - 1, "RE : %s", buf);
+//
+//		int len_r_buf = strnlen_s(r_buf, 128 - 1);
+//
+//
+//
+//		socket.write_some(buffer(r_buf, len_r_buf), r_error);
+//
+//		cout << "보낸 메시지 : " << r_buf << endl;
+//
+//	}
+//
+//
+//
+//	return 0;
+//
+//}
+
+
+
