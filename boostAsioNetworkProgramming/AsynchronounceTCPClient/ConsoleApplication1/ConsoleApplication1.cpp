5b627db9bee080010974c1d11a6ef50dc955d4fc
EeeUnS <42970615+EeeUnS@users.noreply.github.com>
2019-12-26 01:21:21 +0900

    new commit

--- boostAsioNetworkProgramming/AsynchronounceTCPClient/ConsoleApplication1/ConsoleApplication1.cpp
+++ boostAsioNetworkProgramming/AsynchronounceTCPClient/ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,31 +10,24 @@ const unsigned short PORT_NUMBER = 31400;
 class TCP_Client
 {
 public:
-	TCP_Client(boost::asio::io_service& io_service)
+	TCP_Client(boost::asio::io_context& io_service)
 		:m_io_service(io_service),
 		m_Socket(io_service),
-		m_nSeqNumber(0),
-		m_ReceiveBuffer({0,})
+		m_nSeqNumber(0)
+		,m_ReceiveBuffer({0,})
 	{}
 	//~TCP_Client();
 
 	void Connect(boost::asio::ip::tcp::endpoint& endpoint)
 	{
-		m_Socket.async_connect(endpoint,
-			boost::bind(&TCP_Client::handle_connect,
-				this,
-				boost::asio::placeholders::error)
-		);
+		m_Socket.async_connect( endpoint,
+							boost::bind(&TCP_Client::handle_connect,
+								this,
+								boost::asio::placeholders::error)
+							);
 	}
 
 private:
-	boost::asio::io_context& m_io_service;
-	boost::asio::ip::tcp::socket m_Socket;
-	int m_nSeqNumber;
-	std::array<char, 128> m_ReceiveBuffer;
-	std::string m_WriteMessage;
-
-
 	void PostWrite()
 	{
 		if (m_Socket.is_open() == false)
@@ -67,8 +60,8 @@ private:
 	{
 		memset(&m_ReceiveBuffer, '\0', sizeof(m_ReceiveBuffer));
 
-		m_Socket.async_read_some(boost::asio::buffer(m_WriteMessage),
-			boost::bind(&TCP_Client::handle_write, this,
+		m_Socket.async_read_some(boost::asio::buffer(m_ReceiveBuffer),
+			boost::bind(&TCP_Client::handle_receive, this,
 				boost::asio::placeholders::error,
 				boost::asio::placeholders::bytes_transferred)
 		);
@@ -91,7 +84,7 @@ private:
 
 	void handle_write(const boost::system::error_code& /*error*/, std::size_t /*bytes_transferred*/){	}
 	
-	void handle_receive(const boost::system::error_code& error, std::size_t bytes_transferred) 
+	void handle_receive(const boost::system::error_code& error, std::size_t bytes_transferred)
 	{
 		if (error)
 		{
@@ -104,7 +97,7 @@ private:
 			{
 				std::cout << "error No :" << error.value() << " error Message:"
 					<< error.message() << std::endl;
- 			}
+			}
 		}
 		else
 		{
@@ -114,13 +107,18 @@ private:
 			PostWrite();
 		}
 	}
-
+	
+	boost::asio::io_context& m_io_service;
+	boost::asio::ip::tcp::socket m_Socket;
+	int m_nSeqNumber;
+	std::array<char, 128> m_ReceiveBuffer;
+	std::string m_WriteMessage;
 };
 
 int main()
 {
 	boost::asio::io_service io_service;
-	boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string(SERVER_IP), PORT_NUMBER);
+	boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::make_address(SERVER_IP), PORT_NUMBER);
 	
 	TCP_Client client(io_service);
 	client.Connect(endpoint);
---
+++ boostAsioNetworkProgramming/synchronousTCPServer - 복사본/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,57 @@
+﻿
+//ex 3-1 synchronousTCPServer
+#include <iostream>
+#include <boost/asio.hpp>
+//using namespace std;
+//using namespace boost::asio;
+
+const char SERVER_IP[] = "127.0.0.1";
+const unsigned short PORT_NUMBER = 31400;
+
+int main()
+{
+	//클라이언트 접속 준비 핵심
+	boost::asio::io_service io_service;
+	//네트워크 주소 설정
+	boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), PORT_NUMBER);
+	//클라이언트 접속 받기
+	boost::asio::ip::tcp::acceptor acceptor(io_service, endpoint);
+	boost::asio::ip::tcp::socket socket(io_service);
+	acceptor.accept(socket);
+
+	std::cout << "클라이언트 접속" << std::endl;
+		 
+	for (;;)
+	{
+		std::array<char, 128> buf = { 0, };
+
+		//error code wrapping class
+		boost::system::error_code error;
+		std::size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+		if (error)
+		{
+			if (error == boost::asio::error::eof)
+			{
+				std::cout << "클라이언트와 연결이 끊어졌습니다." << std::endl;
+			}
+			else
+			{
+				std::cout << " error No:" << error.value() << "error Message: " << error.message() << std::endl;
+			}
+			break;
+		}
+
+		std::cout << "클라이언트에서 받은 메세지" << &buf[0] << std::endl;
+		
+		char szMessage[128] = { 0, };
+		sprintf_s(szMessage, 128 - 1, "Re:%s", &buf[0]);
+		std::size_t nMsgLen = strnlen_s(szMessage, 128 - 1);
+
+		boost::system::error_code ignored_error;
+		socket.write_some(boost::asio::buffer(szMessage, nMsgLen), ignored_error);
+		std::cout << "클라이언트에 보낸 메시지: " << szMessage << std::endl;
+	}
+	getchar();
+	return 0;
+}
